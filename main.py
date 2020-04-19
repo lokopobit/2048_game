3d4ae7b4320506f82dc9251f8bd9e1c04952cd5c
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-04-19 16:35:41 +0200

    Add: instantiates and object
    in the variable game is loaded a object to play with

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 16:29:53 2020
+
+@author: lokopobit
+"""
+
+# Load internal libreries
+import auxFuns
+from game2048 import Game2048
+
+# Game is the object to play with
+board = auxFuns.random_board(4)
+game = Game2048(board)
+
+
+
