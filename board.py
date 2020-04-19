444d5373597ed2149cb5840d2f75222559d4f8da
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-04-19 15:21:30 +0200

    Add: new methods
    symmetric, transpose and blank_squares

--- board.py
+++ board.py
@@ -25,4 +25,64 @@ class Board(object):
         """
         
         self.board = board
-    
+        
+    def transpose(self):
+        """
+        Returns the transposed board.
+        
+        Example:
+            >>> board.transpose()
+            [[2, 0], [0, 2]]
+        """
+        
+        m = len(self.board)
+        n = len(self.board[0])
+        transposed = auxFuns.createMatrix(n, m)
+        for i in range(n):
+            for j in range(m):
+                transposed[i][j] = self.board[j][i]
+
+        self.board = transposed
+        
+    def symmetric(self):
+        """
+        Returns the symmetric board along the vertical axis.
+        
+        Example:
+            >>> board.symmetric()
+            [[0, 2], [2, 0]]
+        """
+        
+        n = len(self.board)
+        sym = auxFuns.createMatrix(n, n)
+        i = 0
+        while i < n:
+            j = n - 1
+            while j >= 0:
+                sym[i][j] = self.board[i][n-j-1]
+                j -= 1
+            i += 1
+
+        self.board = sym
+        
+    def blank_squares(self):
+        """
+        Returns the empty cells of a board.
+        
+        Example:
+            >>> board.blank_squares()
+            [(0, 1), (1, 0)]
+        """
+        
+        l = []
+        i = 0
+        while i < len(self.board):
+            j = 0
+            while j < len(self.board):
+                if self.board[i][j] == 0:
+                    l.append((i, j))
+                j += 1
+            i += 1
+        return l
+        
+        
